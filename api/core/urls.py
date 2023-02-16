85fe2d70d5ca7d4cc051165104b8114beb13d7ee
Scott Johnson <scottjohnson@Scotts-MBP.usen.ad.jp>
2023-02-16 17:22:19 +0900

    Add api endpoint to get authenticated user

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PokemonViewSet, TypeViewset
+from .views import PokemonViewSet, TypeViewset, logged_in_user
 
 router = DefaultRouter()
 router.register(r'pokemon', PokemonViewSet)
 router.register(r'types', TypeViewset)
-
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("me", logged_in_user)
 ]
--- api/core/views.py
+++ api/core/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 from rest_framework import viewsets
 from .serializers import PokemonSerializer, TypeSerializer
 from .models import Pokemon, Type
-
+from django.contrib.auth.models import User
 class PokemonViewSet(viewsets.ModelViewSet):
     serializer_class = PokemonSerializer
     queryset = Pokemon.objects.all()
@@ -11,3 +10,9 @@ class PokemonViewSet(viewsets.ModelViewSet):
 class TypeViewset(viewsets.ModelViewSet):
     serializer_class = TypeSerializer
     queryset = Type.objects.all()
+
+def logged_in_user(request):
+    if request.user.is_authenticated:
+        user = User.objects.get(username=request.user)
+        return HttpResponse(user)
+    return HttpResponse()
