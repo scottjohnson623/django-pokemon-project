84956e6b01ed79ee091fefef208902c6a9ae8095
Scott Johnson <scottjohnson@Scotts-MBP.usen.ad.jp>
2023-02-17 13:49:43 +0900

    Add endpoint to allow users to favorite a pokemon

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PokemonViewSet, TypeViewset, logged_in_user_view
+from .views import PokemonViewSet, TypeViewset, logged_in_user_view, favorite_pokemon
 
 router = DefaultRouter()
 router.register(r"pokemon", PokemonViewSet)
 router.register(r"types", TypeViewset)
-urlpatterns = [path("", include(router.urls)), path("me", logged_in_user_view)]
+urlpatterns = [path("", include(router.urls)), path("me", logged_in_user_view), path(f'favorite_pokemon', favorite_pokemon)]
--- api/core/views.py
+++ api/core/views.py
@@ -1,7 +1,9 @@
-from django.http.response import JsonResponse
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework import viewsets
 from .serializers import PokemonSerializer, TypeSerializer, UserSerializer
-from .models import Pokemon, Type
+from .models import Pokemon, Type, UserFavoritePokemon
 
 
 class PokemonViewSet(viewsets.ModelViewSet):
@@ -13,8 +15,16 @@ class TypeViewset(viewsets.ModelViewSet):
     serializer_class = TypeSerializer
     queryset = Type.objects.all()
 
-
+@api_view(['GET'])
 def logged_in_user_view(request):
     if request.user.is_authenticated:
-        return JsonResponse(UserSerializer(request.user).data)
-    return JsonResponse()
+        return Response(UserSerializer(request.user).data)
+    return Response()
+
+@api_view(['POST'])
+def favorite_pokemon(request):
+    data = request.data
+    print(data)
+    UserFavoritePokemon.objects.get_or_create(user=request.user, pokemon=Pokemon.objects.get(id=data['pokemon_id']))
+    
+    return Response(status= status.HTTP_200_OK)
