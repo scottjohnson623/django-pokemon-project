ccd37a55cc4bcbd101dc6a2ccbbd5e0a98a462c5
Scott Johnson <scottjohnson@Scotts-MBP.usen.ad.jp>
2023-02-18 21:59:15 +0900

    Feat: add endpoint to fetch a user's favorite pokemons

--- api/core/models/user_favorite_pokemon.py
+++ api/core/models/user_favorite_pokemon.py
@@ -2,18 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from .pokemon import Pokemon
 
-
-class UserFavoritePokemonManager(models.Manager):
-    def create_pokemon(self, name, image_url, pokedex_number):
-        pokemon = self.create(
-            name=name, image_url=image_url, pokedex_number=pokedex_number
-        )
-
-        return pokemon
-
-
 class UserFavoritePokemon(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     date_favorited = models.DateField(auto_now_add=True)
-    objects = UserFavoritePokemonManager()
--- api/core/urls.py
+++ api/core/urls.py
@@ -6,6 +6,7 @@ from .views import (
     logged_in_user_view,
     favorite_pokemon,
     unfavorite_pokemon,
+    user_favorite_pokemon_view
 )
 
 router = DefaultRouter()
@@ -16,4 +17,5 @@ urlpatterns = [
     path("me", logged_in_user_view),
     path(f"favorite_pokemon", favorite_pokemon),
     path(f"unfavorite_pokemon", unfavorite_pokemon),
+    path(f"favorites", user_favorite_pokemon_view),
 ]
